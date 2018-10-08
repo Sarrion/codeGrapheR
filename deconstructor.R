fb4ee27a2d60d83b70d071517058e68a7e67c860
Sarrion <39489130+Sarrion@users.noreply.github.com>
2018-10-08 14:05:13 +0200

    deconstructor() function script created

---
+++ deconstructor.R
@@ -0,0 +1,32 @@
+
+
+deconstructor <- function(string){
+  if( !grepl("\\(", x) && !grepl("\\[", x) ){
+    data.frame(name = string,
+               layer = l,
+               element = e,
+               pointer = p[length(p)], 
+               identifier = id,
+               cluster = cl) ->> graph_data[nrow(graph_data) + 1, ]
+    l <<- l - 1
+    return(0)
+  }
+  if(  grepl("\\(", string) && !grepl("\\[", string) ){
+    roundFirst(string)
+    return(0)
+  }
+  if( !grepl("\\(", string) &&  grepl("\\[", string) ){
+    squareFirst(string)
+    return(0)
+  }
+  if(  grepl("\\(", string) &&  grepl("\\[", string) ){
+    if(str_locate(x, "\\(")[,1] < str_locate(x, "\\[")[,1]){
+      roundFirst(string)
+      return(0)
+    }
+    else{
+      squareFirst(string)
+      return(0)
+    }
+  }
+}
